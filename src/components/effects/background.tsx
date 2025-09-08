43ea3a9b5306aa9fd1db4ebb0c1620c06c171418
DEEP-17 <pateldeep17042004@gmail.com>
2025-09-08 11:19:10 +0530

    everything done

--- src/components/effects/background.tsx
+++ src/components/effects/background.tsx
@@ -1,3 +1,11 @@
-export function Background() 
-{ return ( <div className="fixed inset-0 -z-10 bg-black sm:bg-[radial-gradient(circle_at_center,_rgb(200,220,255,0.08)_0%,_rgb(50,70,100,0.06)_50%,_rgb(10,10,10)_100%)]" /> 
-); } 
+// export function Background() 
+// { return ( <div className="fixed inset-0 -z-10 bg-black sm:bg-[radial-gradient(circle_at_center,_rgb(200,220,255,0.08)_0%,_rgb(50,70,100,0.06)_50%,_rgb(10,10,10)_100%)]" /> 
+// ); } 
+export function Background() {
+    return (
+      <div
+        className="fixed inset-0 -z-10 bg-black sm:bg-[radial-gradient(circle_at_center,_rgba(220,240,255,0.3)_0%,_rgba(50,70,100,0.07)_50%,_rgba(10,10,10,1)_100%)]"
+      />
+    );
+  }
+  
